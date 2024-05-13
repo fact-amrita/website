8a25268a55ac448b45a91a2eae0db2605ee52c10
Dhivijit Koppuravuri <68179868+dhivijit@users.noreply.github.com>
2024-05-13 04:59:01 +0000

    Initialized Next App

---
+++ src/app/app/page.tsx
@@ -0,0 +1,9 @@
+export default function MainAPP() {
+    return (
+        <>
+            <div style={{ display: "inline-block" }}>
+                <h1>This is the route where all the application development will be done</h1>
+            </div>
+        </>
+    );
+}
