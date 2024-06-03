5bb85d20f2bd7e2677ba9eadf44b5a368cf45e08
Dhivijit Koppuravuri <68179868+dhivijit@users.noreply.github.com>
2024-06-03 06:17:50 +0000

    Vuln clearance from snyk

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,7 @@ export default auth((req) => {
     if (req.auth) {
         if (req.nextUrl.pathname === "/app/auth/login") {
             const url = req.url.replace(req.nextUrl.pathname, "/app/details")
+            // deepcode ignore OR: The redirect URL is already mentioned by the previous line and is not changable from the client side
             return Response.redirect(url)
         }
     }
