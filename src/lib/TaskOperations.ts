b1ec0521471492674d62c9285459eae10aa665e5
Dhivijit Koppuravuri <68179868+dhivijit@users.noreply.github.com>
2024-06-13 17:10:35 +0000

    Created Task Operations

---
+++ src/lib/TaskOperations.ts
@@ -0,0 +1,53 @@
+import { db } from "@/lib/db";
+import { TaskIdGen } from "@/functions/taskIdgen";
+
+export async function TaskCreate(taskName: string, taskDescription: string, points: number, domain: string, taskDeadline: string) {
+    // generate a task id and proceed if it does not exist in the database
+    let taskId = TaskIdGen();
+    let task = await db.tasks.findFirst({
+        where: {
+            id: taskId
+        }
+    });
+    while (task) {
+        taskId = TaskIdGen();
+        task = await db.tasks.findFirst({
+            where: {
+                id: taskId
+            }
+        });
+    }
+
+    // create the task
+    await db.tasks.create({
+        data: {
+            TaskId: taskId,
+            task: taskName,
+            description: taskDescription,
+            points: points,
+            domain: domain,
+            deadline: taskDeadline
+        }
+    })
+
+    return taskId;
+}
+
+export async function TaskDelete(taskId: string) {
+    // check if task exists
+    const task = await db.tasks.findFirst({
+        where: {
+            TaskId: taskId
+        }
+    });
+
+    if (!task) {
+        throw new Error("Task does not exist");
+    } else {
+        await db.tasks.delete({
+            where: {
+                TaskId: taskId
+            }
+        });
+    }
+}
