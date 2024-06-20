5d53f4964c1a0065609fbc27d592d1b2e745c3c5
Dhivijit Koppuravuri <68179868+dhivijit@users.noreply.github.com>
2024-06-20 18:19:04 +0000

    Grouped routes

---
+++ src/app/(application)/app/layout.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from "next";
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div>
+            <main>
+                {children}
+            </main>
+        </div>
+    );
+}
