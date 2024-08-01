c9f6ef8b8b979a6c9009344a898fdef9c6baf29c
HeathKnowles <heathknowles662@gmail.com>
2024-08-01 16:42:20 +0530

    About FACT

--- src/components/homepage/About.tsx
+++ src/components/homepage/About.tsx
@@ -8,7 +8,25 @@ function About() {
         <div className="wave"></div>
         <div className="wave"></div>
         <div className="infotop"><br />FACT<br />
-          <div className="name">MikeAndrewDesigner</div>
+          <div className="name">
+            The Forensic Analysis Club and Triage (FACT) at Amrita Vishwa Vidyapeetham 
+            is dedicated to preparing students for careers in cyber forensics, ethical 
+            hacking, penetration testing, and forensics science. The club's vision is to 
+            develop tools and applications to solve cybercrimes through forensics 
+            investigation. Their mission includes promoting advanced security standards 
+            and technology adoption, collaborating with forensics experts to create 
+            investigative tools, educating members about forensic science, and 
+            encouraging ethical behavior and professionalism.The club values honesty, 
+            integrity, social responsibility, logical reasoning, and teamwork. 
+            Their activities involve conducting Forensics Investigation Challenges (FIC),
+             organizing cyber awareness programs, and exposing students to physical 
+             investigation techniques. They also work on developing software applications
+              for cyber forensics, setting up a Forensics Investigation Lab for training 
+              and certification, and conducting research to enhance investigative 
+              procedures.In the future, FACT envisions offering consultancy services f
+              or evidence analysis and preservation, exploring behavior forensics, 
+              cognitive analytics, and Cyber Warfare
+              </div>
         </div>
       </div>
     </section>
