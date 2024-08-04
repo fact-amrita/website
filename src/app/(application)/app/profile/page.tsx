b8eaa98c8b5d3e6189ea51120fc06e3a3027aa73
HeathKnowles <heathknowles662@gmail.com>
2024-08-04 10:17:06 +0530

    Moved factid localstorage to useeffect hook

--- src/app/(application)/app/profile/page.tsx
+++ src/app/(application)/app/profile/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React, { useState, useEffect } from 'react';
 import { usersFind } from "@/lib/UserFetch";
@@ -16,21 +16,31 @@ interface Result {
 }
 
 const ProfilePage: React.FC = () => {
-
   const [results, setResults] = useState<Result[]>([]);
+  const [storedFactId, setStoredFactId] = useState<string | null>(null);
 
   useEffect(() => {
     const loadDefaultResults = async () => {
-      const searchResults = await usersFind('');
-      setResults(searchResults);
-    }
+      try {
+        const searchResults = await usersFind('');
+        setResults(searchResults);
+      } catch (error) {
+        console.error("Error loading default results:", error);
+      }
+    };
+
     loadDefaultResults();
+    setStoredFactId(window.localStorage.getItem('factId'));
   }, []);
 
   const handleSearch = async (event: React.ChangeEvent<HTMLInputElement>) => {
-    const searchResults = await usersFind(event.target.value);
-    setResults(searchResults);
-  }
+    try {
+      const searchResults = await usersFind(event.target.value);
+      setResults(searchResults);
+    } catch (error) {
+      console.error("Error handling search:", error);
+    }
+  };
 
   return (
     <div className="container mx-auto mt-8 px-4">
@@ -48,7 +58,7 @@ const ProfilePage: React.FC = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 ml-2">
           {results.map(result => (
             <Link href={`/app/profile/${result.FactID}`} key={result.id}>
-              <div className={`border border-gray-300 p-3 rounded-tr-3xl rounded-bl-3xl bg-white shadow-xl hover:shadow-2xl transition-shadow duration-300 ${result.FactID === window.localStorage.getItem('factId') ? 'shadow-slate-600 shadow-xl hover:shadow-yellow-600' : ''}`}
+              <div className={`border border-gray-300 p-3 rounded-tr-3xl rounded-bl-3xl bg-white shadow-xl hover:shadow-2xl transition-shadow duration-300 ${result.FactID === storedFactId ? 'shadow-slate-600 shadow-xl hover:shadow-yellow-600' : ''}`}
                 style={{ width: '100%', height: '150px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
                 <div className="text-center">
                   <Image src={result.image ?? ''} alt={result.name} width={70} height={70} className="w-10 rounded-full mb-2" />
