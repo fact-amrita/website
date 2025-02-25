c354d926f65a6a05ed49ec3dad38eaa7d12d2b6d
Dhivijit K <unknown@example.com>
2025-02-25 22:09:44 +0530

    Using speedinsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  <SpeedInsights />
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
